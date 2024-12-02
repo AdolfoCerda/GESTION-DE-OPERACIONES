31ac711f19d8edab1ddd5d1ea52ebf986a3864da
AdolfoCerda <143052847+AdolfoCerda@users.noreply.github.com>
2024-12-01 20:38:24 -0700

    Update para corregir registro reportes getEquipos.php

--- reportes/getEquipos.php
+++ reportes/getEquipos.php
@@ -1,8 +1,11 @@
 <?php
 include 'config.php'; // Conexión a la base de datos
 
-if (isset($_POST['id_ubicacion'])) {
-    $id_ubicacion = $_POST['id_ubicacion'];
+if (isset($_POST['nombre_ubicacion'])) {
+
+    // Obtener id_ubicacion y nombre_ubicacion
+    $nombre_ubicacion = $_POST['nombre_ubicacion'];
+    $id_ubicacion = $conn->query("SELECT id_ubicacion FROM Ubicaciones WHERE nombre_ubicacion = '$nombre_ubicacion'")->fetch_assoc()['id_ubicacion'];
 
     // Consultar los equipos en la ubicación seleccionada
     $sql = "SELECT numero_serie FROM Equipos WHERE id_ubicacion = ?";
