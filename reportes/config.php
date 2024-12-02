383bfe7558a243d2692ed2fa9e025244578e0153
AdolfoCerda <143052847+AdolfoCerda@users.noreply.github.com>
2024-12-01 20:59:50 -0700

    Obtener tipo_usuario en config.php

--- reportes/config.php
+++ reportes/config.php
@@ -12,4 +12,14 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
+
+//obtener tipo de usuario
+$sql = "SELECT tipo_usuario FROM Usuarios WHERE nombre_usuario = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("s", $_SESSION['usuario']);
+$stmt->execute();
+$result = $stmt->get_result();
+$tipo_usuario = $result->fetch_assoc()['tipo_usuario'];
+$stmt->close();
+
 ?>
