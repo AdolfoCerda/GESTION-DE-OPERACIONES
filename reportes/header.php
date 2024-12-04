91ff2e17a8dfc0d04259df7b94143b0477cf6632
jessicaacostaaguirre <143643248+jessicaacostaaguirre@users.noreply.github.com>
2024-12-04 00:38:26 -0700

    agregar saludo con nombre de usuario

--- reportes/header.php
+++ reportes/header.php
@@ -4,6 +4,14 @@
             <img src="imagenes/logo.png" alt="Logo de la Universidad" class="logo" style="max-width: 150px; margin-right:-100px;">
             <h1 class="ms-3 text-center" style="flex-grow: 1;">Control de Mantenimiento y Servicios</h1>
         </div>
+        <div style="text-align: right;">
+            <?php
+                // Verificar si existe un usuario en la sesión
+                if (isset($_SESSION['usuario'])) {
+                    echo '<p class="mb-0 ms-3">Hola, ' . htmlspecialchars($_SESSION['usuario']) . '!</p>';
+                }
+            ?>
+        </div>
         <nav>
             <ul class="nav justify-content-center mt-3">
                 <li class="nav-item"><a href="inicio.php" class="nav-link">Inicio</a></li>
